d3d2ffbd748f934da7311f4a63e4d93c190880c5
Adam <adam@phenaproxima.net>
2018-02-15 11:16:52 -0500

    Add a little more unit test coverage (#12)

---
+++ tests/src/Unit/UpdateManagerTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\Tests\lightning_core\Unit;
+
+use Drupal\Component\Plugin\Discovery\DiscoveryInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\DependencyInjection\ClassResolver;
+use Drupal\lightning_core\UpdateManager;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @coversDefaultClass \Drupal\lightning_core\UpdateManager
+ *
+ * @group lightning
+ * @group lightning_core
+ */
+class UpdateManagerTest extends UnitTestCase {
+
+  /**
+   * @covers ::getAvailable
+   */
+  public function testGetAvailable() {
+    $discovery = $this->prophesize(DiscoveryInterface::class);
+    $discovery->getDefinitions()->willReturn([
+      'fubar:1.2.1' => [
+        'id' => '1.2.1',
+        'provider' => 'fubar',
+      ],
+      'fubar:1.2.2' => [
+        'id' => '1.2.2',
+        'provider' => 'fubar',
+      ],
+      'fubar:1.2.3' => [
+        'id' => '1.2.3',
+        'provider' => 'fubar',
+      ],
+    ]);
+
+    $update_manager = new UpdateManager(
+      new \ArrayIterator,
+      new ClassResolver,
+      $this->prophesize(ConfigFactoryInterface::class)->reveal(),
+      $discovery->reveal());
+
+    $definitions = $update_manager->getAvailable('1.2.2');
+    $this->assertCount(1, $definitions);
+    $this->assertArrayHasKey('fubar:1.2.3', $definitions);
+  }
+
+  /**
+   * @covers ::toSemanticVersion
+   *
+   * @dataProvider providerSemanticVersion
+   */
+  public function testSemanticVersion($drupal_version, $semantic_version) {
+    $this->assertSame($semantic_version, UpdateManager::toSemanticVersion($drupal_version));
+  }
+
+  public function providerSemanticVersion() {
+    return [
+      ['8.x-1.12', '1.12.0'],
+      ['8.x-1.2-alpha3', '1.2.0-alpha3'],
+      ['8.x-2.7-beta3', '2.7.0-beta3'],
+      ['8.x-1.42-rc1', '1.42.0-rc1'],
+      ['8.x-1.x-dev', '1.x-dev'],
+      // This is a weird edge case only used by the Lightning profile.
+      ['8.x-3.001', '3.001.0'],
+    ];
+  }
+
+}
